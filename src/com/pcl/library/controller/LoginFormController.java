e274c30e4e8b7f1d86472fd51c356da158bd4338
Gihanvs98 <darkcreationsl98@gmail.com>
2025-08-03 23:48:08 +0530

    feat:navigattion implemented login to signup

--- src/com/pcl/library/controller/LoginFormController.java
+++ src/com/pcl/library/controller/LoginFormController.java
@@ -1,4 +1,30 @@
 package com.pcl.library.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+    public AnchorPane context;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void signupOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignupForm");
+    }
+    private void setUi(String location) throws IOException {
+       Stage stage =(Stage) context.getScene().getWindow();
+       stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+
+    }
 }
--- src/com/pcl/library/controller/SignupFormController.java
+++ src/com/pcl/library/controller/SignupFormController.java
@@ -1,4 +1,19 @@
 package com.pcl.library.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignupFormController {
+    public TextField txtFirstName;
+    public TextField txtLastName;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+
+    public void saveUserOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
