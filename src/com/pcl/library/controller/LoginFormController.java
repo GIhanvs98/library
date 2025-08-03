a028609f1280df2832ae63a2c191c4d3394fc4e8
Gihanvs98 <darkcreationsl98@gmail.com>
2025-08-04 00:46:53 +0530

    feat:login mehtod implemented

--- src/com/pcl/library/controller/LoginFormController.java
+++ src/com/pcl/library/controller/LoginFormController.java
@@ -1,8 +1,11 @@
 package com.pcl.library.controller;
 
+import com.pcl.library.db.Database;
+import com.pcl.library.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -15,7 +18,23 @@ public class LoginFormController {
     public PasswordField txtPassword;
     public AnchorPane context;
 
-    public void loginOnAction(ActionEvent actionEvent) {
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+      String email = txtEmail.getText().toLowerCase();
+      String password=txtPassword.getText().trim();
+
+      for (User user:Database.usersTable){
+          if(user.getEmail().equals(email)){
+              if(user.getPassword().equals(password)){
+                  setUi("Dashboard");
+              }else {
+                  new Alert(Alert.AlertType.ERROR,"Invalid Password!").show();
+                  return;
+              }
+          }else {
+              new Alert(Alert.AlertType.ERROR,"Invalid Email!").show();
+          }
+
+      }
 
     }
 
