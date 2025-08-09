226e33cbbce940968ee3fab50e9dcf487a9facb3
Gihanvs98 <darkcreationsl98@gmail.com>
2025-08-10 01:07:33 +0530

    feat:book save implemented

--- src/com/pcl/library/controller/DashboardController.java
+++ src/com/pcl/library/controller/DashboardController.java
@@ -1,4 +1,30 @@
 package com.pcl.library.controller;
 
+import com.pcl.library.db.Database;
+import com.pcl.library.model.Book;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class DashboardController {
+    public TextField txtBId;
+    public TextField txtBName;
+    public TextField txtAuthor;
+    public TextField txtCupboard;
+    public TextField txtSection;
+    public TextField txtAvailability;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+       Book book= new Book(
+                txtBId.getText(),
+                txtBName.getText(),
+                txtAuthor.getText(),
+                txtCupboard.getText(),
+                txtSection.getText(),
+                txtAvailability.getText()
+                );
+        Database.bookTable.add(book);
+        new Alert(Alert.AlertType.INFORMATION,"New Book Added").show();
+        System.out.println(book.toString());
+    }
 }
---
+++ src/com/pcl/library/model/Book.java
@@ -0,0 +1,86 @@
+package com.pcl.library.model;
+
+public class Book {
+   private String bookID;
+   private String bookName;
+   private  String bookAuthor;
+   private String cupboard;
+   private String section;
+    private String availability;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID='" + bookID + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", cupboard='" + cupboard + '\'' +
+                ", section='" + section + '\'' +
+                ", availability='" + availability + '\'' +
+                '}';
+    }
+
+    public Book() {
+    }
+
+    public Book(String bookID, String bookName, String bookAuthor, String cupboard, String section, String availability) {
+        this.bookID = bookID;
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.cupboard = cupboard;
+        this.section = section;
+        this.availability = availability;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public void setCupboard(String cupboard) {
+        this.cupboard = cupboard;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getCupboard() {
+        return cupboard;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+
+
+
+}
